445cafb12732770a44941d327c1563952f4b8e76
Bogdan Pali <bogdan.pali@trakosoft.ro>
2023-09-15 17:41:30 +0300

    [master] carti and versete entities added

---
+++ app/Models/Carti.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Model;
+
+class Carti extends Model
+{
+    protected $table = 'biblia_carti';
+
+    protected $connection = 'mysql';
+
+}
---
+++ app/Models/Versete.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Model;
+
+class Versete extends Model
+{
+    protected $table = 'biblia_versete';
+
+    protected $connection = 'mysql';
+
+}
